0ebf9c8464c143baef184dbde6ae63fcef32d29a
jjk23 <37400378+jjk23@users.noreply.github.com>
2023-01-30 10:55:24 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include<map>
+#include<stdlib.h>
+#include<string>
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout.precision(6);
+    double r;
+    double pi = 3.14159265358979;
+    cin >> r;
+    cout << r * r * pi<<'\n';
+    cout << r * r * 2;
+}
